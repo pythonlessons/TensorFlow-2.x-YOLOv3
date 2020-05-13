f65403d57a209b5e2f850d05e8ac3b8137ed1701
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-05-13 11:12:05 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 #
 #   File name   : train.py
 #   Author      : PyLessons
-#   Created date: 2020-05-05
+#   Created date: 2020-05-13
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : used to train custom object detector
@@ -14,7 +14,6 @@ import shutil
 import numpy as np
 import tensorflow as tf
 #from tensorflow.keras.utils import plot_model
-#from tqdm import tqdm
 from yolov3.dataset import Dataset
 from yolov3.yolov3 import Create_Yolov3, YOLOv3, decode, compute_loss
 from yolov3.utils import load_yolo_weights
@@ -25,7 +24,7 @@ logdir = TRAIN_LOGDIR
 Darknet_weights = YOLO_DARKNET_WEIGHTS
 
 save_best_only = True # saves only best agent according validation loss
-save_checkpoints = False # saves all best validates checkpoints in training process (may require a lot disk space)
+save_checkpoints = False # saves all best validated checkpoints in training process (may require a lot disk space)
 
 if os.path.exists(logdir): shutil.rmtree(logdir)
 writer = tf.summary.create_file_writer(logdir)
@@ -42,8 +41,14 @@ if TRAIN_TRANSFER:
     load_yolo_weights(Darknet, Darknet_weights) # use darknet weights
 
 yolo = Create_Yolov3(input_size=input_size, training=True, CLASSES=TRAIN_CLASSES)
+if TRAIN_FROM_CHECKPOINT:
+    try:
+        yolo.load_weights(TRAIN_FROM_CHECKPOINT)
+    except ValueError:
+        print("Shapes are incompatible, transfering Darknet weights")
+        TRAIN_FROM_CHECKPOINT = False
 
-if TRAIN_TRANSFER:
+if TRAIN_TRANSFER and not TRAIN_FROM_CHECKPOINT:
     for i, l in enumerate(Darknet.layers):
         layer_weights = l.get_weights()
         if layer_weights != []:
@@ -76,7 +81,7 @@ def train_step(image_data, target):
         # update learning rate
         # about warmup: https://arxiv.org/pdf/1812.01187.pdf&usg=ALkJrhglKOPDjNt6SHGbphTHyMcT0cuMJg
         global_steps.assign_add(1)
-        if global_steps < warmup_steps and not TRAIN_TRANSFER:
+        if global_steps < warmup_steps:# and not TRAIN_TRANSFER:
             lr = global_steps / warmup_steps * TRAIN_LR_INIT
         else:
             lr = TRAIN_LR_END + 0.5 * (TRAIN_LR_INIT - TRAIN_LR_END)*(
@@ -109,14 +114,6 @@ def validate_step(image_data, target):
             prob_loss += loss_items[2]
 
         total_loss = giou_loss + conf_loss + prob_loss
-        # writing summary data
-        with validate_writer.as_default():
-            tf.summary.scalar("lr", optimizer.lr, step=global_steps)
-            tf.summary.scalar("validate_loss/total_loss", total_loss, step=global_steps)
-            tf.summary.scalar("validate_loss/giou_loss", giou_loss, step=global_steps)
-            tf.summary.scalar("validate_loss/conf_loss", conf_loss, step=global_steps)
-            tf.summary.scalar("validate_loss/prob_loss", prob_loss, step=global_steps)
-        validate_writer.flush()
         
     return giou_loss.numpy(), conf_loss.numpy(), prob_loss.numpy(), total_loss.numpy()
 
@@ -130,7 +127,12 @@ for epoch in range(TRAIN_EPOCHS):
         print("epoch:{:2.0f} step:{:5.0f}/{}, lr:{:.6f}, giou_loss:{:7.2f}, conf_loss:{:7.2f}, prob_loss:{:7.2f}, total_loss:{:7.2f}"
               .format(epoch, cur_step, steps_per_epoch, results[1], results[2], results[3], results[4], results[5]))
 
-    count, giou_val, conf_val, prob_val, total_val = 0, 0, 0, 0, 0
+    if len(testset) == 0:
+        print("configure TEST options to validate model")
+        yolo.save_weights("./checkpoints/yolov3_custom")
+        continue
+    
+    count, giou_val, conf_val, prob_val, total_val = 0., 0, 0, 0, 0
     for image_data, target in testset:
         results = validate_step(image_data, target)
         count += 1
@@ -138,10 +140,18 @@ for epoch in range(TRAIN_EPOCHS):
         conf_val += results[1]
         prob_val += results[2]
         total_val += results[3]
+    # writing validate summary data
+    with validate_writer.as_default():
+        tf.summary.scalar("validate_loss/total_val", total_val/count, step=epoch)
+        tf.summary.scalar("validate_loss/giou_val", giou_val/count, step=epoch)
+        tf.summary.scalar("validate_loss/conf_val", conf_val/count, step=epoch)
+        tf.summary.scalar("validate_loss/prob_val", prob_val/count, step=epoch)
+    validate_writer.flush()
+        
     print("\n\ngiou_val_loss:{:7.2f}, conf_val_loss:{:7.2f}, prob_val_loss:{:7.2f}, total_val_loss:{:7.2f}\n\n".
           format(giou_val/count, conf_val/count, prob_val/count, total_val/count))
 
-    if save_checkpoints and not save_checkpoints:
+    if save_checkpoints and not save_best_only:
         yolo.save_weights("./checkpoints/yolov3_custom"+"_val_loss_{:7.2f}".format(total_val/count))
     if save_best_only and best_val_loss>total_val/count:
         yolo.save_weights("./checkpoints/yolov3_custom")
