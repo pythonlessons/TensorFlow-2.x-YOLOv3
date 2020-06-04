d5b07dd49d0ec6eb9ed748eb7e875a6f7c9d7631
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-06-04 16:32:55 +0300

    Added everything to functions
    Added everything to functions

--- tools/XML_to_YOLOv3.py
+++ tools/XML_to_YOLOv3.py
@@ -2,7 +2,7 @@
 #
 #   File name   : XML_to_YOLOv3.py
 #   Author      : PyLessons
-#   Created date: 2020-05-012
+#   Created date: 2020-06-04
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : used to convert XML labels to YOLOv3 training labels
@@ -16,11 +16,11 @@ foldername = os.path.basename(os.getcwd())
 if foldername == "tools": os.chdir("..")
 
 
-data_dir = '/model_data/Dataset/'
-Dataset_names_path = "model_data/Dataset_names.txt"
-Dataset_train = "model_data/Dataset_train.txt"
-Dataset_test = "model_data/Dataset_test.txt"
-is_subfolder = True
+data_dir = '/custom_dataset/'
+Dataset_names_path = "model_data/license_plate_names.txt"
+Dataset_train = "model_data/license_plate_train.txt"
+Dataset_test = "model_data/license_plate_test.txt"
+is_subfolder = False
 
 Dataset_names = []
       
@@ -46,20 +46,21 @@ def ParseXML(img_folder, file):
         print(img_path)
         file.write(img_path+'\n')
 
+def run_XML_to_YOLOv3():
+    for i, folder in enumerate(['train','test']):
+        with open([Dataset_train,Dataset_test][i], "w") as file:
+            print(os.getcwd()+data_dir+folder)
+            img_path = os.path.join(os.getcwd()+data_dir+folder)
+            if is_subfolder:
+                for directory in os.listdir(img_path):
+                    xml_path = os.path.join(img_path, directory)
+                    ParseXML(xml_path, file)
+            else:
+                ParseXML(img_path, file)
 
-for i, folder in enumerate(['train','test']):
-    with open([Dataset_train,Dataset_test][i], "w") as file:
-        print(os.getcwd()+data_dir+folder)
-        img_path = os.path.join(os.getcwd()+data_dir+folder)
-        if is_subfolder:
-            for directory in os.listdir(img_path):
-                xml_path = os.path.join(img_path, directory)
-                ParseXML(xml_path, file)
-        else:
-            ParseXML(img_path, file)
+    print("Dataset_names:", Dataset_names)
+    with open(Dataset_names_path, "w") as file:
+        for name in Dataset_names:
+            file.write(str(name)+'\n')
 
-
-print("Dataset_names:", Dataset_names)
-with open(Dataset_names_path, "w") as file:
-    for name in Dataset_names:
-        file.write(str(name)+'\n')
+run_XML_to_YOLOv3()
--- tools/oid_to_pascal_voc_xml.py
+++ tools/oid_to_pascal_voc_xml.py
@@ -2,7 +2,7 @@
 #
 #   File name   : oid_to_pascal_vos_xml.py
 #   Author      : PyLessons
-#   Created date: 2020-05-05
+#   Created date: 2020-06-04
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : used to convert oid labels to pascal vos xml
@@ -21,152 +21,155 @@ if foldername == "tools": os.chdir("..")
 
 Dataset_path = "OIDv4_ToolKit/OID/Dataset"
 
-current_path = os.getcwd()
-os.chdir(Dataset_path)
-DIRS = os.listdir(os.getcwd())
+def convert_to_xml():
+    current_path = os.getcwd()
+    os.chdir(Dataset_path)
+    DIRS = os.listdir(os.getcwd())
 
-for DIR in DIRS:
-    if os.path.isdir(DIR):
-        os.chdir(DIR)
+    for DIR in DIRS:
+        if os.path.isdir(DIR):
+            os.chdir(DIR)
 
-        print("Currently in Subdirectory:", DIR)
-        CLASS_DIRS = os.listdir(os.getcwd()) 
-        for CLASS_DIR in CLASS_DIRS:
-            if " " in CLASS_DIR:
-                os.rename(CLASS_DIR, CLASS_DIR.replace(" ", "_"))
-        
-        CLASS_DIRS = os.listdir(os.getcwd())
-        for CLASS_DIR in CLASS_DIRS:
-            if os.path.isdir(CLASS_DIR):
-                os.chdir(CLASS_DIR)
+            print("Currently in Subdirectory:", DIR)
+            CLASS_DIRS = os.listdir(os.getcwd()) 
+            for CLASS_DIR in CLASS_DIRS:
+                if " " in CLASS_DIR:
+                    os.rename(CLASS_DIR, CLASS_DIR.replace(" ", "_"))
+            
+            CLASS_DIRS = os.listdir(os.getcwd())
+            for CLASS_DIR in CLASS_DIRS:
+                if os.path.isdir(CLASS_DIR):
+                    os.chdir(CLASS_DIR)
 
-                print("\n" + "Creating PASCAL VOC XML Files for Class:", CLASS_DIR)
-                # Create Directory for annotations if it does not exist yet
+                    print("\n" + "Creating PASCAL VOC XML Files for Class:", CLASS_DIR)
+                    # Create Directory for annotations if it does not exist yet
 
-                #Read Labels from OIDv4 ToolKit
-                os.chdir("Label")
+                    #Read Labels from OIDv4 ToolKit
+                    os.chdir("Label")
 
-                #Create PASCAL XML
-                for filename in tqdm(os.listdir(os.getcwd())):
-                    if filename.endswith(".txt"):
-                        filename_str = str.split(filename, ".")[0]
+                    #Create PASCAL XML
+                    for filename in tqdm(os.listdir(os.getcwd())):
+                        if filename.endswith(".txt"):
+                            filename_str = str.split(filename, ".")[0]
 
 
-                        annotation = etree.Element("annotation")
-                        
-                        os.chdir("..")
-                        folder = etree.Element("folder")
-                        folder.text = os.path.basename(os.getcwd())
-                        annotation.append(folder)
+                            annotation = etree.Element("annotation")
+                            
+                            os.chdir("..")
+                            folder = etree.Element("folder")
+                            folder.text = os.path.basename(os.getcwd())
+                            annotation.append(folder)
 
-                        filename_xml = etree.Element("filename")
-                        filename_xml.text = filename_str + ".jpg"
-                        annotation.append(filename_xml)
+                            filename_xml = etree.Element("filename")
+                            filename_xml.text = filename_str + ".jpg"
+                            annotation.append(filename_xml)
 
-                        path = etree.Element("path")
-                        path.text = os.path.join(os.path.dirname(os.path.abspath(filename)), filename_str + ".jpg")
-                        annotation.append(path)
+                            path = etree.Element("path")
+                            path.text = os.path.join(os.path.dirname(os.path.abspath(filename)), filename_str + ".jpg")
+                            annotation.append(path)
 
-                        source = etree.Element("source")
-                        annotation.append(source)
+                            source = etree.Element("source")
+                            annotation.append(source)
 
-                        database = etree.Element("database")
-                        database.text = "Unknown"
-                        source.append(database)
+                            database = etree.Element("database")
+                            database.text = "Unknown"
+                            source.append(database)
 
-                        size = etree.Element("size")
-                        annotation.append(size)
+                            size = etree.Element("size")
+                            annotation.append(size)
 
-                        width = etree.Element("width")
-                        height = etree.Element("height")
-                        depth = etree.Element("depth")
+                            width = etree.Element("width")
+                            height = etree.Element("height")
+                            depth = etree.Element("depth")
 
-                        img = cv2.imread(filename_xml.text)
+                            img = cv2.imread(filename_xml.text)
 
-                        try:
-                            width.text = str(img.shape[1])
-                        except AttributeError:
+                            try:
+                                width.text = str(img.shape[1])
+                            except AttributeError:
+                                os.chdir("Label")
+                                continue
+                            height.text = str(img.shape[0])
+                            depth.text = str(img.shape[2])
+
+                            size.append(width)
+                            size.append(height)
+                            size.append(depth)
+
+                            segmented = etree.Element("segmented")
+                            segmented.text = "0"
+                            annotation.append(segmented)
+
                             os.chdir("Label")
-                            continue
-                        height.text = str(img.shape[0])
-                        depth.text = str(img.shape[2])
+                            label_original = open(filename, 'r')
 
-                        size.append(width)
-                        size.append(height)
-                        size.append(depth)
+                            # Labels from OIDv4 Toolkit: name_of_class X_min Y_min X_max Y_max
+                            for line in label_original:
+                                line = line.strip()
+                                l = line.split(' ')
+                                
+                                class_name_len = len(l) - 4 # 4 coordinates
+                                class_name = l[0]
+                                for i in range(1,class_name_len):
+                                    class_name = f"{class_name}_{l[i]}"
 
-                        segmented = etree.Element("segmented")
-                        segmented.text = "0"
-                        annotation.append(segmented)
+                                addi = class_name_len
 
-                        os.chdir("Label")
-                        label_original = open(filename, 'r')
+                                xmin_l = str(int(round(float(l[0+addi]))))
+                                ymin_l = str(int(round(float(l[1+addi]))))
+                                xmax_l = str(int(round(float(l[2+addi]))))
+                                ymax_l = str(int(round(float(l[3+addi]))))
+                                
+                                obj = etree.Element("object")
+                                annotation.append(obj)
 
-                        # Labels from OIDv4 Toolkit: name_of_class X_min Y_min X_max Y_max
-                        for line in label_original:
-                            line = line.strip()
-                            l = line.split(' ')
-                            
-                            class_name_len = len(l) - 4 # 4 coordinates
-                            class_name = l[0]
-                            for i in range(1,class_name_len):
-                                class_name = f"{class_name}_{l[i]}"
+                                name = etree.Element("name")
+                                name.text = class_name
+                                obj.append(name)
 
-                            addi = class_name_len
+                                pose = etree.Element("pose")
+                                pose.text = "Unspecified"
+                                obj.append(pose)
 
-                            xmin_l = str(int(round(float(l[0+addi]))))
-                            ymin_l = str(int(round(float(l[1+addi]))))
-                            xmax_l = str(int(round(float(l[2+addi]))))
-                            ymax_l = str(int(round(float(l[3+addi]))))
-                            
-                            obj = etree.Element("object")
-                            annotation.append(obj)
+                                truncated = etree.Element("truncated")
+                                truncated.text = "0"
+                                obj.append(truncated)
 
-                            name = etree.Element("name")
-                            name.text = class_name
-                            obj.append(name)
+                                difficult = etree.Element("difficult")
+                                difficult.text = "0"
+                                obj.append(difficult)
 
-                            pose = etree.Element("pose")
-                            pose.text = "Unspecified"
-                            obj.append(pose)
+                                bndbox = etree.Element("bndbox")
+                                obj.append(bndbox)
 
-                            truncated = etree.Element("truncated")
-                            truncated.text = "0"
-                            obj.append(truncated)
+                                xmin = etree.Element("xmin")
+                                xmin.text = xmin_l
+                                bndbox.append(xmin)
 
-                            difficult = etree.Element("difficult")
-                            difficult.text = "0"
-                            obj.append(difficult)
+                                ymin = etree.Element("ymin")
+                                ymin.text = ymin_l
+                                bndbox.append(ymin)
 
-                            bndbox = etree.Element("bndbox")
-                            obj.append(bndbox)
+                                xmax = etree.Element("xmax")
+                                xmax.text = xmax_l
+                                bndbox.append(xmax)
 
-                            xmin = etree.Element("xmin")
-                            xmin.text = xmin_l
-                            bndbox.append(xmin)
+                                ymax = etree.Element("ymax")
+                                ymax.text = ymax_l
+                                bndbox.append(ymax)
 
-                            ymin = etree.Element("ymin")
-                            ymin.text = ymin_l
-                            bndbox.append(ymin)
+                            os.chdir("..")
+                            # write xml to file
+                            s = etree.tostring(annotation, pretty_print=True)
+                            with open(filename_str + ".xml", 'wb') as f:
+                                f.write(s)
+                                f.close()
 
-                            xmax = etree.Element("xmax")
-                            xmax.text = xmax_l
-                            bndbox.append(xmax)
+                            os.chdir("Label")
 
-                            ymax = etree.Element("ymax")
-                            ymax.text = ymax_l
-                            bndbox.append(ymax)
+                    os.chdir("..")
+                    os.chdir("..")   
+                       
+            os.chdir("..")
 
-                        os.chdir("..")
-                        # write xml to file
-                        s = etree.tostring(annotation, pretty_print=True)
-                        with open(filename_str + ".xml", 'wb') as f:
-                            f.write(s)
-                            f.close()
-
-                        os.chdir("Label")
-
-                os.chdir("..")
-                os.chdir("..")   
-                   
-        os.chdir("..")
+convert_to_xml()
