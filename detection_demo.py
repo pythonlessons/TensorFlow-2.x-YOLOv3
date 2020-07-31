56c29070fad11df4a77fd28651d0d0a00b3d94bd
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-07-31 10:48:38 +0300

    Update detection_demo.py

--- detection_demo.py
+++ detection_demo.py
@@ -2,7 +2,7 @@
 #
 #   File name   : detection_demo.py
 #   Author      : PyLessons
-#   Created date: 2020-05-08
+#   Created date: 2020-07-27
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : object detection image and video example
@@ -13,21 +13,22 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
 import numpy as np
 import tensorflow as tf
-from yolov3.yolov3 import Create_Yolov3
+#from yolov3.yolov3 import Create_Yolov3
+from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
 from yolov3.configs import *
 
-input_size = YOLO_INPUT_SIZE
-Darknet_weights = YOLO_DARKNET_WEIGHTS
-if TRAIN_YOLO_TINY:
-    Darknet_weights = YOLO_DARKNET_TINY_WEIGHTS
+if YOLO_TYPE == "yolov4":
+    Darknet_weights = YOLO_V4_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V4_WEIGHTS
+if YOLO_TYPE == "yolov3":
+    Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
 
 image_path   = "./IMAGES/kite.jpg"
-video_path   = "./IMAGES/street_drive.mp4"
+video_path   = "./IMAGES/test.mp4"
 
-yolo = Create_Yolov3(input_size=input_size)
+yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
 load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
 
-detect_image(yolo, image_path, '', input_size=input_size, show=True, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, '', input_size=input_size, show=True, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=input_size, show=True, rectangle_colors=(255, 0, 0))
+#detect_image(yolo, image_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
