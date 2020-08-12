ca15cce43bfef5c8fc1334507ce9760e836eab22
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-08-12 14:00:20 +0300

    Update detect_mnist.py

--- detect_mnist.py
+++ detect_mnist.py
@@ -2,7 +2,7 @@
 #
 #   File name   : detect_mnist.py
 #   Author      : PyLessons
-#   Created date: 2020-04-20
+#   Created date: 2020-08-12
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : mnist object detection example
@@ -15,11 +15,9 @@ import numpy as np
 import random
 import time
 import tensorflow as tf
-from yolov3.yolov3 import Create_Yolov3
+from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import detect_image
 from yolov3.configs import *
-
-input_size=YOLO_INPUT_SIZE
 
 while True:
     ID = random.randint(0, 200)
@@ -28,9 +26,7 @@ while True:
 
     image_path = image_info[0]
 
-    yolo = Create_Yolov3(input_size=input_size, CLASSES=TRAIN_CLASSES)
-    yolo.load_weights("./checkpoints/yolov3_custom_Tiny") # use keras weights
+    yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
+    yolo.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}") # use keras weights
 
-    detect_image(yolo, image_path, "mnist_test.jpg", input_size=input_size, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-    time.sleep(5)
-
+    detect_image(yolo, image_path, "mnist_test.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
