f18268f2c25a4d1f8d06ef78feb6d17dd34a1385
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-08-13 16:33:20 +0300

    Update detection_custom.py

--- detection_custom.py
+++ detection_custom.py
@@ -1,28 +1,32 @@
 #================================================================
 #
-#   File name   : detection_demo.py
+#   File name   : detection_custom.py
 #   Author      : PyLessons
-#   Created date: 2020-05-18
+#   Created date: 2020-08-13
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : object detection image and video example
 #
 #================================================================
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
 import numpy as np
 import tensorflow as tf
-from yolov3.yolov3 import Create_Yolov3
+#from yolov3.yolov3 import Create_Yolov3
+from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
 from yolov3.configs import *
 
-Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
+if YOLO_TYPE == "yolov4":
+    Darknet_weights = YOLO_V4_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V4_WEIGHTS
+if YOLO_TYPE == "yolov3":
+    Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
 
 image_path   = "./IMAGES/plate_2.jpg"
 video_path   = "./IMAGES/test.mp4"
 
-yolo = Create_Yolov3(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
+yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
 yolo.load_weights("./checkpoints/yolov3_custom") # use keras weights
 
 detect_image(yolo, image_path, "./IMAGES/plate_1_detect.jpg", input_size=YOLO_INPUT_SIZE, show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
