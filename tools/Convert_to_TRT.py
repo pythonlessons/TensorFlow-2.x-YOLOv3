b1d6f6c082455f99309a09ed08f4b686fe1d8052
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-08-18 09:08:31 +0300

    Add files via upload

---
+++ tools/Convert_to_TRT.py
@@ -0,0 +1,52 @@
+#================================================================
+#
+#   File name   : Convert_to_TRT.py
+#   Author      : PyLessons
+#   Created date: 2020-08-17
+#   Website     : https://pylessons.com/
+#   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
+#   Description : convert TF frozen graph to TensorRT model
+#
+#================================================================
+
+# https://medium.com/tensorflow/speed-up-tensorflow-inference-on-gpus-with-tensorrt-13b49f3db3fa
+# https://medium.com/tensorflow/speed-up-tensorflow-inference-on-gpus-with-tensorrt-13b49f3db3fa
+# https://medium.com/@sujit.kumar731/conversion-of-keras-model-to-tensorrt-model-443c92801622
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import sys
+
+foldername = os.path.basename(os.getcwd())
+if foldername == "tools":
+    os.chdir("..")
+    sys.path.insert(1, os.getcwd())
+    
+import tensorflow as tf
+import numpy as np
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from yolov3.configs import *
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+
+def calibration_input():
+    for i in range(10):
+        batched_input = np.random.random((1, YOLO_INPUT_SIZE, YOLO_INPUT_SIZE, 3)).astype(np.float32)
+        batched_input = tf.constant(batched_input)
+        yield (batched_input,)
+
+conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS
+conversion_params = conversion_params._replace(max_workspace_size_bytes=4000000000)
+conversion_params = conversion_params._replace(precision_mode=YOLO_TRT_QUANTIZE_MODE)
+conversion_params = conversion_params._replace(max_batch_size=1)
+if YOLO_TRT_QUANTIZE_MODE == 'INT8':
+    conversion_params = conversion_params._replace(use_calibration=True)
+
+converter = trt.TrtGraphConverterV2(input_saved_model_dir=f'./checkpoints/{YOLO_TYPE}-{YOLO_INPUT_SIZE}', conversion_params=conversion_params)
+if YOLO_TRT_QUANTIZE_MODE == 'INT8':
+    converter.convert(calibration_input_fn=calibration_input)
+else:
+    converter.convert()
+
+converter.save(output_saved_model_dir=f'./checkpoints/{YOLO_TYPE}-trt-{YOLO_TRT_QUANTIZE_MODE}-{YOLO_INPUT_SIZE}')
+print(f'Done Converting to TensorRT, model saved to: /checkpoints/{YOLO_TYPE}-trt-{YOLO_TRT_QUANTIZE_MODE}-{YOLO_INPUT_SIZE}')
---
+++ tools/Convert_to_pb.py
@@ -0,0 +1,42 @@
+#================================================================
+#
+#   File name   : Convert_to_pb.py
+#   Author      : PyLessons
+#   Created date: 2020-08-17
+#   Website     : https://pylessons.com/
+#   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
+#   Description : used to freeze tf model to .pb model
+#
+#================================================================
+# https://medium.com/tensorflow/speed-up-tensorflow-inference-on-gpus-with-tensorrt-13b49f3db3fa
+# https://medium.com/tensorflow/speed-up-tensorflow-inference-on-gpus-with-tensorrt-13b49f3db3fa
+# https://medium.com/@sujit.kumar731/conversion-of-keras-model-to-tensorrt-model-443c92801622
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import sys
+
+foldername = os.path.basename(os.getcwd())
+if foldername == "tools":
+    os.chdir("..")
+    sys.path.insert(1, os.getcwd())
+
+import tensorflow as tf
+from yolov3.yolov4 import Create_Yolo
+from yolov3.utils import load_yolo_weights
+from yolov3.configs import *
+
+if YOLO_TYPE == "yolov4":
+    Darknet_weights = YOLO_V4_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V4_WEIGHTS
+if YOLO_TYPE == "yolov3":
+    Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
+
+yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
+if YOLO_CUSTOM_WEIGHTS == False:
+    load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
+else:
+    yolo.load_weights(YOLO_CUSTOM_WEIGHTS) # use custom weights
+
+yolo.summary()
+yolo.save(f'./checkpoints/{YOLO_TYPE}-{YOLO_INPUT_SIZE}')
+
+print(f"model saves to /checkpoints/{YOLO_TYPE}-{YOLO_INPUT_SIZE}")
