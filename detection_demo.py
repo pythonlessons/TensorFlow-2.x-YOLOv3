53b952faeb1d6aa29dfe7923bfe083550020ea6e
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-08-20 15:23:23 +0300

    Added TensorRT support
    Added TensorRT support

--- detection_demo.py
+++ detection_demo.py
@@ -2,7 +2,7 @@
 #
 #   File name   : detection_demo.py
 #   Author      : PyLessons
-#   Created date: 2020-07-27
+#   Created date: 2020-08-14
 #   Website     : https://pylessons.com/
 #   GitHub      : https://github.com/pythonlessons/TensorFlow-2.x-YOLOv3
 #   Description : object detection image and video example
@@ -13,21 +13,26 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import cv2
 import numpy as np
 import tensorflow as tf
-#from yolov3.yolov3 import Create_Yolov3
+from tensorflow.python.saved_model import tag_constants
 from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
 from yolov3.configs import *
 
-if YOLO_TYPE == "yolov4":
-    Darknet_weights = YOLO_V4_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V4_WEIGHTS
-if YOLO_TYPE == "yolov3":
-    Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
-
 image_path   = "./IMAGES/kite.jpg"
 video_path   = "./IMAGES/test.mp4"
 
-yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
-load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
+if YOLO_FRAMEWORK == "tf": # TensorFlow detection
+    if YOLO_TYPE == "yolov4":
+        Darknet_weights = YOLO_V4_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V4_WEIGHTS
+    if YOLO_TYPE == "yolov3":
+        Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
+    yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
+    load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
+    
+elif YOLO_FRAMEWORK == "trt": # TensorRT detection
+    saved_model_loaded = tf.saved_model.load(YOLO_CUSTOM_WEIGHTS, tags=[tag_constants.SERVING])
+    signature_keys = list(saved_model_loaded.signatures.keys())
+    yolo = saved_model_loaded.signatures['serving_default']
 
 detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
 #detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
