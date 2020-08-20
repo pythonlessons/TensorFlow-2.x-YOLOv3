a0dba6cac396b1d7d1fc309d2b3430d46ce3f50f
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-08-20 15:39:58 +0300

    Update configs.py

--- yolov3/configs.py
+++ yolov3/configs.py
@@ -20,7 +20,6 @@ YOLO_TRT_QUANTIZE_MODE      = "INT8" # INT8, FP16, FP32
 YOLO_CUSTOM_WEIGHTS         = False # "checkpoints/yolov3_custom" # used in evaluate_mAP.py, if not using leave False
                             # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection
 YOLO_COCO_CLASSES           = "model_data/coco/coco.names"
-YOLO_COCO_CLASSES           = "model_data/coco/coco.names"
 YOLO_STRIDES                = [8, 16, 32]
 YOLO_IOU_LOSS_THRESH        = 0.5
 YOLO_ANCHOR_PER_SCALE       = 3
