1545aad4ca8f26f55e783c1b644ce53a809451d3
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-09-16 23:21:36 +0300

    Update detection_demo.py

--- detection_demo.py
+++ detection_demo.py
@@ -15,17 +15,13 @@ import numpy as np
 import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
-import time
 
 image_path   = "./IMAGES/kite.jpg"
 video_path   = "./IMAGES/test.mp4"
 
-t1 = time.time()
 yolo = Load_Yolo_model()
 detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
 #detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
 
 #detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
-t2 = time.time()
-print(t2-t1)
