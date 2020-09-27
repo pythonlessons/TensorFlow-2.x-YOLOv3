d1e4402aa32d879e03dbd0b9e339af6a315930a9
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2020-09-27 22:19:22 +0300

    Update utils.py

--- yolov3/utils.py
+++ yolov3/utils.py
@@ -294,6 +294,7 @@ def detect_image(Yolo, image_path, output_path, input_size=416, show=False, CLAS
     bboxes = nms(bboxes, iou_threshold, method='nms')
 
     image = draw_bbox(original_image, bboxes, CLASSES=CLASSES, rectangle_colors=rectangle_colors)
+    # CreateXMLfile("XML_Detections", str(int(time.time())), image, bboxes, read_class_names(CLASSES))
 
     if output_path != '': cv2.imwrite(output_path, image)
     if show:
@@ -480,6 +481,7 @@ def detect_video(Yolo, video_path, output_path, input_size=416, show=False, CLAS
         fps2 = 1000 / (sum(times_2)/len(times_2)*1000)
         
         image = cv2.putText(image, "Time: {:.1f}FPS".format(fps), (0, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+        # CreateXMLfile("XML_Detections", str(int(time.time())), image, bboxes, read_class_names(CLASSES))
         
         print("Time: {:.2f}ms, Detection FPS: {:.1f}, total FPS: {:.1f}".format(ms, fps, fps2))
         if output_path != '': out.write(image)
@@ -542,6 +544,7 @@ def detect_realtime(Yolo, output_path, input_size=416, show=False, CLASSES=YOLO_
         print("Time: {:.2f}ms, {:.1f} FPS".format(ms, fps))
 
         frame = draw_bbox(original_frame, bboxes, CLASSES=CLASSES, rectangle_colors=rectangle_colors)
+        # CreateXMLfile("XML_Detections", str(int(time.time())), frame, bboxes, read_class_names(CLASSES))
         image = cv2.putText(frame, "Time: {:.1f}FPS".format(fps), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
 
